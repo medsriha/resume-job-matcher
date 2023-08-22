6ef0f899dac94e24d853efb29f38f52e4fbc9f91
mohamed sriha <medsriha@live.fr>
2023-08-21 20:15:51 -0500

    NTR

--- scripts/user_profile.py
+++ scripts/user_profile.py
@@ -8,143 +8,150 @@ import spacy
 from utils import TextCleaner
 
 # Load the English model
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 
 RESUME_SECTIONS = {
-    'Accomplishments',
-    'Achievements and Accomplishments',
-    'Additional Achievements',
-    'Additional Information',
-    'Availability',
-    'Awards',
-    'Awards and Honors',
-    'Awards and Recognition',
-    'Certifications',
-    'Computer Skills',
-    'Conferences and Presentations',
-    'Contact Information',
-    'Courses Relevant to the Job',
-    'Education',
-    'Education and Training',
-    'Employment History',
-    'Experience',
-    'Extracurricular Activities',
-    'Grants and Scholarships',
-    'Highlights',
-    'Honors',
-    'Interest and Hobbies',
-    'Interests and Hobbies',
-    'Internship Experience',
-    'Language Skills',
-    'Languages',
-    'Leadership Experience',
-    'Licenses',
-    'Memberships Honors',
-    'Memberships and Honors',
-    'Military Service',
-    'Objective',
-    'Personal Information',
-    'Personal Projects',
-    'Professional Affiliations',
-    'Professional DevelopmentResearch Experience',
-    'Professional Experience',
-    'Professional GoalsTechnical Skills',
-    'Professional Memberships',
-    'Professional Skills',
-    'Programming Languages',
-    'Project Portfolio',
-    'Projects',
-    'Public Speaking Engagements',
-    'Publications',
-    'Qualifications',
-    'References',
-    'References from LinkedIn',
-    'Research Experience',
-    'Salary History',
-    'Skills',
-    'Soft Skills',
-    'Software Skills',
-    'Summary',
-    'Summary of Qualifications',
-    'Teaching Experience',
-    'Technical Expertise',
-    'Technical Proficiencies',
-    'Transferable Skills',
-    'Volunteer Experience',
-    'Volunteer Work',
-    'Volunteering',
-    'Work Experience',
-    'Work History',
-    'QUALIFICATIONS',
-    'ACCOMPLISHMENTS',
-    'SOFT SKILLS',
-    'RESEARCH EXPERIENCE',
-    'PERSONAL INFORMATION',
-    'CONFERENCES AND PRESENTATIONS',
-    'ADDITIONAL INFORMATION',
-    'WORK HISTORY',
-    'INTERESTS AND HOBBIES',
-    'CERTIFICATIONS',
-    'PROGRAMMING LANGUAGES',
-    'PROFESSIONAL DEVELOPMENTRESEARCH EXPERIENCE',
-    'PROFESSIONAL SKILLS',
-    'HIGHLIGHTS',
-    'INTEREST AND HOBBIES',
-    'PROJECT PORTFOLIO',
-    'REFERENCES',
-    'MEMBERSHIPS AND HONORS',
-    'SOFTWARE SKILLS',
-    'TEACHING EXPERIENCE',
-    'EDUCATION AND TRAINING',
-    'PUBLICATIONS',
-    'VOLUNTEERING',
-    'PROFESSIONAL GOALSTECHNICAL SKILLS',
-    'CONTACT INFORMATION',
-    'ACHIEVEMENTS AND ACCOMPLISHMENTS',
-    'SUMMARY OF QUALIFICATIONS',
-    'PROFESSIONAL MEMBERSHIPS',
-    'REFERENCES FROM LINKEDIN',
-    'VOLUNTEER EXPERIENCE',
-    'LANGUAGES',
-    'VOLUNTEER WORK',
-    'TECHNICAL EXPERTISE',
-    'AWARDS AND RECOGNITION',
-    'LICENSES',
-    'INTERNSHIP EXPERIENCE',
-    'OBJECTIVE',
-    'MEMBERSHIPS HONORS',
-    'PUBLIC SPEAKING ENGAGEMENTS',
-    'LEADERSHIP EXPERIENCE',
-    'EMPLOYMENT HISTORY',
-    'GRANTS AND SCHOLARSHIPS',
-    'SUMMARY',
-    'HONORS',
-    'AWARDS AND HONORS',
-    'COMPUTER SKILLS',
-    'EXTRACURRICULAR ACTIVITIES',
-    'PROFESSIONAL EXPERIENCE',
-    'EXPERIENCE',
-    'TECHNICAL PROFICIENCIES',
-    'PERSONAL PROJECTS',
-    'SALARY HISTORY',
-    'TRANSFERABLE SKILLS',
-    'ADDITIONAL ACHIEVEMENTS',
-    'LANGUAGE SKILLS',
-    'AWARDS',
-    'PROFESSIONAL AFFILIATIONS',
-    'COURSES RELEVANT TO THE JOB',
-    'SKILLS',
-    'AVAILABILITY',
-    'MILITARY SERVICE',
-    'EDUCATION',
-    'WORK EXPERIENCE',
-    'PROJECTS'}
+    "Accomplishments",
+    "Achievements and Accomplishments",
+    "Additional Achievements",
+    "Additional Information",
+    "Availability",
+    "Awards",
+    "Awards and Honors",
+    "Awards and Recognition",
+    "Certifications",
+    "Computer Skills",
+    "Conferences and Presentations",
+    "Contact Information",
+    "Courses Relevant to the Job",
+    "Education",
+    "Education and Training",
+    "Employment History",
+    "Experience",
+    "Extracurricular Activities",
+    "Grants and Scholarships",
+    "Highlights",
+    "Honors",
+    "Interest and Hobbies",
+    "Interests and Hobbies",
+    "Internship Experience",
+    "Language Skills",
+    "Languages",
+    "Leadership Experience",
+    "Licenses",
+    "Memberships Honors",
+    "Memberships and Honors",
+    "Military Service",
+    "Objective",
+    "Personal Information",
+    "Personal Projects",
+    "Professional Affiliations",
+    "Professional Development", 
+    "Research Experience",
+    "Professional Experience",
+    "Professional Goals",
+    "Technical Skills",
+    "Professional Memberships",
+    "Professional Skills",
+    "Programming Languages",
+    "Project Portfolio",
+    "Projects",
+    "Public Speaking Engagements",
+    "Publications",
+    "Qualifications",
+    "References",
+    "References from LinkedIn",
+    "Research Experience",
+    "Salary History",
+    "Skills",
+    "Soft Skills",
+    "Software Skills",
+    "Summary",
+    "Summary of Qualifications",
+    "Teaching Experience",
+    "Technical Expertise",
+    "Technical Proficiencies",
+    "Transferable Skills",
+    "Volunteer Experience",
+    "Volunteer Work",
+    "Volunteering",
+    "Work Experience",
+    "Work History",
+    "QUALIFICATIONS",
+    "ACCOMPLISHMENTS",
+    "SOFT SKILLS",
+    "RESEARCH EXPERIENCE",
+    "PERSONAL INFORMATION",
+    "CONFERENCES AND PRESENTATIONS",
+    "ADDITIONAL INFORMATION",
+    "WORK HISTORY",
+    "INTERESTS AND HOBBIES",
+    "CERTIFICATIONS",
+    "PROGRAMMING LANGUAGES",
+    "PROFESSIONAL DEVELOPMENT",
+    "RESEARCH EXPERIENCE",
+    "PROFESSIONAL SKILLS",
+    "HIGHLIGHTS",
+    "INTEREST AND HOBBIES",
+    "PROJECT PORTFOLIO",
+    "REFERENCES",
+    "MEMBERSHIPS AND HONORS",
+    "SOFTWARE SKILLS",
+    "TEACHING EXPERIENCE",
+    "EDUCATION AND TRAINING",
+    "PUBLICATIONS",
+    "VOLUNTEERING",
+    "PROFESSIONAL GOALS",
+    "TECHNICAL SKILLS",
+    "CONTACT INFORMATION",
+    "ACHIEVEMENTS AND ACCOMPLISHMENTS",
+    "SUMMARY OF QUALIFICATIONS",
+    "PROFESSIONAL MEMBERSHIPS",
+    "REFERENCES FROM LINKEDIN",
+    "VOLUNTEER EXPERIENCE",
+    "LANGUAGES",
+    "VOLUNTEER WORK",
+    "TECHNICAL EXPERTISE",
+    "AWARDS AND RECOGNITION",
+    "LICENSES",
+    "INTERNSHIP EXPERIENCE",
+    "OBJECTIVE",
+    "MEMBERSHIPS HONORS",
+    "PUBLIC SPEAKING ENGAGEMENTS",
+    "LEADERSHIP EXPERIENCE",
+    "EMPLOYMENT HISTORY",
+    "GRANTS AND SCHOLARSHIPS",
+    "SUMMARY",
+    "HONORS",
+    "AWARDS AND HONORS",
+    "COMPUTER SKILLS",
+    "EXTRACURRICULAR ACTIVITIES",
+    "PROFESSIONAL EXPERIENCE",
+    "EXPERIENCE",
+    "TECHNICAL PROFICIENCIES",
+    "PERSONAL PROJECTS",
+    "SALARY HISTORY",
+    "TRANSFERABLE SKILLS",
+    "ADDITIONAL ACHIEVEMENTS",
+    "LANGUAGE SKILLS",
+    "AWARDS",
+    "PROFESSIONAL AFFILIATIONS",
+    "COURSES RELEVANT TO THE JOB",
+    "SKILLS",
+    "AVAILABILITY",
+    "MILITARY SERVICE",
+    "EDUCATION",
+    "WORK EXPERIENCE",
+    "PROJECTS"}
 
 REGEX_PATTERNS = {
     "email_pattern": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b",
     "phone_pattern": r"\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}",
-    "link_pattern": r"\b(?:https?://|www\.)\S+\b"
-}
+    "link_pattern": r"\b(?:https?://|www\.)\S+\b",
+    "date_pattern": r"(?:(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s\d{4}|\d{1,2}[ /\.-]\d{4}|present|current|Present|Current)",
+    "experience_pattern": r"(Professional|Work|Internship|Volunteer|Volunteering|Leadership|Research|Teaching|PROFESSIONAL|WORK|INTERNSHIP|VOLUNTEER|VOLUNTEERING|LEADERSHIP|RESEARCH|TEACHING)\s+(Experience|experience|EXPERIENCE|Expertise|expertise|EXPERTISE|History|history|HISTORY|Activities|activities|ACTIVITIES)",
+    "skills_pattern": r"(Technical|Summary|Hard|TECHNICAL|SUMMARY|HARD)(?: of|OF)? (Proficiencies|Qualifications|Expertise|Skills|SKILLS|PROFICIENCIES|QUALIFICATIONS|EXPERTISE)"
+    }
 
 
 class Profile(object):
@@ -160,7 +167,7 @@ class Profile(object):
             self.text = resume[key]
 
         self.doc = nlp(self.text)
-        self.tokens = re.split(r'\n| {4,}', self.text)
+        self.tokens = re.split(r"\n| {4,}", self.text)
         
         self.key = key
         self.cleaner = TextCleaner()
@@ -235,10 +242,8 @@ class Profile(object):
             Returns:
                 list: A list containing the extracted position and year.
         """
-        # This will look for patterns like "Dec 2022," and "January-2022," or "01/2023," "05-2022," and "11.2024"
-        pattern = r"(?:(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s\d{4}|\d{1,2}[ /\.-]\d{4})"
-
-        matches = re.findall(pattern, text)
+    
+        matches = re.findall(REGEX_PATTERNS["date_pattern"], text)
 
         return matches
     
@@ -250,7 +255,7 @@ class Profile(object):
             bool: 
         """
         if text in ("EXPERIENCE", "experience", "Experience", "Accomplishments", "ACCOMPLISHMENTS") or\
-              re.findall(r"(Professional|Work|Internship|Volunteer|Volunteering|Leadership|Research|Teaching|PROFESSIONAL|WORK|INTERNSHIP|VOLUNTEER|VOLUNTEERING|LEADERSHIP|RESEARCH|TEACHING)\s+(Experience|experience|EXPERIENCE|Expertise|expertise|EXPERTISE|History|history|HISTORY|Activities|activities|ACTIVITIES)", text):
+              re.findall(REGEX_PATTERNS["experience_pattern"], text):
             return True
         return False
 
@@ -263,7 +268,7 @@ class Profile(object):
             bool: 
         """
         if text in ("SKILLS", "skills", "Skills", "Qualifications", "QUALIFICATIONS", "Highlights", "HIGHLIGHTS") or\
-              re.findall(r"(Technical|Summary|TECHNICAL|SUMMARY)(?: of|OF)? (Proficiencies|Qualifications|Expertise|PROFICIENCIES|QUALIFICATIONS|EXPERTISE)", text):
+            re.findall(REGEX_PATTERNS["skills_pattern"], text):
             return True
         return False
     
@@ -291,11 +296,11 @@ class Profile(object):
             str: A string containing all the extracted experience.
         """
         if section:
-            if section == 'experience':
+            if section == "experience":
                 fct = self.__is_section_experience
-            elif section == 'skills':
+            elif section == "skills":
                 fct = self.__is_section_skills
-            elif section == 'education':
+            elif section == "education":
                 fct = self.__is_section_education
             else:
                 raise Exception("Section name is unknown. Please provide one of the following input (experience, skills, and education)")
@@ -320,35 +325,43 @@ class Profile(object):
                     # Found section
                     is_section = True
 
-        return " ".join(details)
+        details = " ".join(details)
+
+        if section == 'experience':
+            # Extract dates
+            # for item in details:
+            date = self.__get_position_year(details)
+                # if date:
+            print(date)
+            
+        return details
+        
     
     
     def create(self):
         """
         Return a dictionary with resume data
         """
+        #return [ent.text for ent in nlp(self.get_section(section = "experience")) if ent.label_ == "ORG"]
+
         return {"index": self.key,
                 "name": self.get_names(),
                 "email": self.get_emails(),
                 "phone_number": self.get_phone_numbers(),
                 "links": self.get_links(),
-                "experience": self.get_section(section = 'experience'),
-                "skills": self.get_section(section='skills'),
-                "education": self.get_section(section='education') }
-
-"""
-# Test snipet
-resumes = read_multiple_pdf("resumes//", k = 5)
-"""
+                "experience": self.get_section(section = "experience"),
+                "skills": self.get_section(section="skills"),
+                "education": self.get_section(section="education") }
 
 if __name__ == "__main__":
     from utils import TextExtractor
     import pandas as pd
 
-    path = "C:\\Users\\medSr\\Documents\\resume\\resumes\\MISC\\MohamedSriha-Discord.pdf"
-    #path ="C:\\Users\\medSr\\Documents\\resume\\resumes\\DESIGNER\\22675694.pdf"
+    path = "C:\\Users\\medSr\\Documents\\resume-job-matcher\\resumes\\MISC\\MohamedSriha-Discord.pdf"
+    path ="C:\\Users\\medSr\\Documents\\resume-job-matcher\\resumes\\CHEF\\10333299.pdf"
     resume_txt = TextExtractor(file_path = path).convert_pdf()
-    print(Profile(resume=resume_txt, key=path).create()['experience'])
+    Profile(resume=resume_txt, key=path).create()['experience']
+    # print(Profile(resume=resume_txt, key=path).create())
     
     # resume_txt = TextExtractor(file_path = path).convert_pdf()
     # data = [Profile(resume=v, key=k).create() for k, v in resume_txt.items()]
@@ -357,4 +370,4 @@ if __name__ == "__main__":
     # print(Profile(resume=resume_txt, key=path).get()["education"])
     # Profile(resume=resume_txt, key=path).get()
     # pattern = r"(\d{4}))"
-    # print(re.findall(pattern, '2022-present', flags=re.IGNORECASE))
+    # print(re.findall(pattern, "2022-present", flags=re.IGNORECASE))
